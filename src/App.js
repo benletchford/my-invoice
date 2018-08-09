5ad8b80aade88d12e46b1ca72f7fb4f808f2f84a
Ben Letchford <contact@benl.com.au>
2018-08-09 17:31:41 +1000

    Add fonts, css and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import './css/app.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, world!
+        <span class="dark">Hello, world!</span>
       </div>
     )
   }
